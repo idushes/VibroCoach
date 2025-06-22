f68c70e2066a860bbdd64f70590bfd4590a99c90
dushes <dushes.nadym@gmail.com>
2025-06-22 12:58:06 +0800

    Initial Commit

---
+++ VibroCoach Watch App/VibroCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibroCoachApp.swift
+//  VibroCoach Watch App
+//
+//  Created by Andrei Boiko on 22.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct VibroCoach_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VibroCoach/VibroCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibroCoachApp.swift
+//  VibroCoach
+//
+//  Created by Andrei Boiko on 22.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct VibroCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
